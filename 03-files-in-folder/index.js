4c1aed421e3d09b186271033c81b3cb475067062
talestris <meggycleary@gmail.com>
2025-01-23 00:19:29 +0300

    fix: make fix for assignment 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs').promises;
 const filePath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(filePath, (err, files) => {
-  if (err) {
+fs.readdir(filePath)
+  .then((files) => {
+    files.forEach((file) => {
+      fs.stat(path.join(filePath, file))
+        .then((fileStats) => {
+          if (fileStats.isFile()) {
+            const fileName = path.parse(file).name;
+            const fileExtension = path.parse(file).ext.slice(1);
+            const fileSize = (fileStats.size / 1024).toFixed(3) + 'kb';
+
+            console.log(`${fileName} - ${fileExtension} - ${fileSize}`);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  })
+  .catch((err) => {
     console.error(err);
     process.exit(1);
-  }
-
-  files.forEach((file) => {
-    const fileStats = fs.statSync(path.join(filePath, file));
-    if (fileStats.isFile()) {
-      const fileName = path.parse(file).name;
-      const fileExtension = path.parse(file).ext.slice(1);
-      const fileSize = (fileStats.size / 1024).toFixed(3) + 'kb';
-      console.log(`${fileName} - ${fileExtension} - ${fileSize}`);
-    }
   });
-});
